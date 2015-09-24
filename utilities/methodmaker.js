af7e63bcb27a96e158963a533f6d5c50fa7ad1de
Will Ricketts <will@willricketts.com>
2015-09-24 14:05:49 -0400

    added command parser

--- utilities/methodmaker.js
+++ utilities/methodmaker.js
@@ -3,5 +3,20 @@ module.exports = {
 };
 
 function parseCommand(string) {
-  var command = string.substring(1, string.length);
+  // scrape the ! from the first position of the string
+  var commandArray = string.substring(1, string.length).split(' ');
+  var arguments = [];
+
+  // set the command from the first word in the string
+  var command = commandArray[0];
+
+  // gather the arguments after the command
+  for(var i = 1; i <= commandArray.length; i++) {
+    arguments.push(commandArray[i]);
+  }
+
+  return {
+    method: command,
+    arguments: arguments
+  };
 }
