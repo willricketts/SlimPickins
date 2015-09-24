37bae57fcec2926df527ec1494aef5e1c02ccc4a
Will Ricketts <will@willricketts.com>
2015-09-24 18:44:59 -0400

    added proper dogecoin to usd conversion and refactored currency actions

--- actions/cryptocurrencies.js
+++ actions/cryptocurrencies.js
@@ -6,15 +6,27 @@ module.exports = {
 var client = require('../bot.js').client;
 
 function bitcoin() {
-  request("https://bitpay.com/api/rates/usd", function(err, response, body) {
-    var res = JSON.parse(body);
-    client.say('#funhole', '$' + res.rate);
+  getBitcoinPrice(function(price) {
+    client.say('#funhole', '$' + price);
   });
 }
 
 function dogecoin() {
+  getDogecoinPrice(function(dogecoinPrice) {
+    getBitcoinPrice(function(bitcoinPrice) {
+      client.say('#funhole', '$' + (dogecoinPrice * bitcoinPrice));
+    });
+  });
+}
+
+function getBitcoinPrice(callback) {
+  request("https://bitpay.com/api/rates/usd", function(err, response, body) {
+    callback(JSON.parse(body).rate);
+  });
+}
+
+function getDogecoinPrice(callback) {
   request('http://dogecoinaverage.com/BTC.json', function(err, response, body) {
-    var res = JSON.parse(body);
-    client.say('#funhole', 'Ƀ ' + res.markets[0].price);
+    callback(JSON.parse(body).markets[0].price)
   });
 }
